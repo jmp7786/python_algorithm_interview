9690ccadf82e6a8f234effe369ca59e7241c21f2
Jaemin Park <jmp7786@gmail.com>
2023-04-01 10:34:30 +0900

    학습 leetcode 435

--- leetcode/merge-intervals/435.py
+++ leetcode/merge-intervals/435.py
@@ -1,2 +1,16 @@
 # https://leetcode.com/problems/non-overlapping-intervals/
+import sys
+from typing import List
 
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        intervals.sort()
+        result, prev = 0, -sys.maxsize
+        for l, r in intervals:
+            if l < prev:
+                result += 1
+                if r > prev:
+                    continue
+            prev = r
+        return result
