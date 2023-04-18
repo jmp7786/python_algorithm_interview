8cba9051f9c08a461897cadad08c7a5c178764a8
Jaemin Park <jmp7786@gmail.com>
2023-04-18 23:33:42 +0900

    dp 413 학습

---
+++ leetcode/dp/413.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/arithmetic-slices/
+# 스스로 풀었다. 계속 푸니까 직관이 생긴 것 같다.
+from typing import List
+
+
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        count = 0
+        for i in range(2, len(nums)):
+            diff = nums[i - 1] - nums[i - 2]
+            if diff == nums[i] - nums[i - 1]:
+                count += 1
+                for j in range(i + 1, len(nums)):
+                    if diff == nums[j] - nums[j - 1]:
+                        count += 1
+                    else:
+                        break
+            else:
+                continue
+
+        return count
